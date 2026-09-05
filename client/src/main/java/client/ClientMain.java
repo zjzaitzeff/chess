233d4c751f73b8a9ce3f4580b774cb8d2cb8bb09
zjzaitzeff <zjzaitzeff@gmail.com>
2026-09-05 12:16:52 -0600

    Initial commit

---
+++ client/src/main/java/client/ClientMain.java
@@ -0,0 +1,10 @@
+package client;
+
+import chess.*;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        System.out.println("♕ 240 Chess Client: " + piece);
+    }
+}
---
+++ server/src/main/java/server/ServerMain.java
@@ -0,0 +1,10 @@
+package server;
+
+import chess.*;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        System.out.println("♕ 240 Chess Server: " + piece);
+    }
+}
