9c0dd5aa9f86e30e3d885c5c21331c9819acd668
Zane Zaitzeff <zjzaitzeff@gmail.com>
2026-09-11 18:18:16 -0600

    isInCheckmate() function

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -113,7 +113,32 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        if (isInCheck(teamColor)) {
+            for (int i = 1; i < 9; i++) {
+                for (int j = 1; j < 9; j++) {
+                    ChessPosition current_position = new ChessPosition(i, j);
+                    ChessPiece p = the_board.getPiece(current_position);
+                    if (teamColor.equals(p.getTeamColor())) {
+                        Collection<ChessMove> moves = validMoves(current_position);
+                        for (ChessMove move : moves) {
+                            ChessBoard old_board = the_board.copy_board();
+                            ChessPosition sp = move.getStartPosition();
+                            ChessPosition ep = move.getEndPosition();
+                            Collection<ChessMove> valid_moves = validMoves(sp);
+                            if (valid_moves.contains(move)) {
+                                the_board.addPiece(ep, the_board.getPiece(sp));
+                                the_board.addPiece(sp, null);
+                            if (!isInCheck(teamColor)) {
+                                the_board = old_board.copy_board();
+                                return false;
+                            }
+                        }
+                    }
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     /**
